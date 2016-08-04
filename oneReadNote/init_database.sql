2e9a957292dc3c6053f7dc73a37d8c75d5a99f8e
喵喵就是猫 <faustmeow@outlook.com>
2016-08-04 23:26:13 +0800

    create the database and init it

---
+++ oneReadNote/init_database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE readnote;
+
+CREATE TABLE sharers (
+				id  INT NOT NULL AUTO_INCREMENT,
+				name VARCHAR(255) NOT NULL,
+                photo VARCHAR(255) NOT NULL,
+                created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id));
+
+CREATE TABLE notes (
+				id  INT NOT NULL AUTO_INCREMENT,
+				title VARCHAR(255) NOT NULL,
+                content VARCHAR(10000) NOT NULL,
+                readtimes INT NOT NULL DEFAULT 1,
+                praises INT NOT NULL DEFAULT 0,
+                updatetime VARCHAR(15) NOT NULL,
+                created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id));
+
+CREATE TABLE links (
+				id  INT NOT NULL AUTO_INCREMENT,
+				shr_Id INT NOT NULL,
+                note_Id INT NOT NULL,
+                created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id));
